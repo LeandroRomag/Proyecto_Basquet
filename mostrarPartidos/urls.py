d226694d1a142392003b23eade7c9d9c680d16ce
alerrego <rodriguezregoalejandro@yahoo.com.ar>
2024-12-29 19:35:25 -0300

    cammbios-leandro

--- mostrarPartidos/urls.py
+++ mostrarPartidos/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path ('', views.verPartidos),
-<<<<<<< HEAD
     path ('ayer/', views.verPartidosAyer)
-=======
-    path ('manana/', views.verPartidosManana)
-    
->>>>>>> leandro
 ]
--- mostrarPartidos/views.py
+++ mostrarPartidos/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render
 from nba_api.live.nba.endpoints import scoreboard
 from nba_api.stats.endpoints import scoreboardv2
-<<<<<<< HEAD
 
 from datetime import datetime, timedelta
 import json
-import wikipedia
 
-
-
-=======
-from datetime import datetime, timedelta
-from nba_api.stats.static import teams
-from datetime import date
->>>>>>> leandro
 # Create your views here.
-f = "{gameId}: {awayTeam} {score1} vs. {score2} {homeTeam}"
-team_dict = {team["id"]: team["full_name"] for team in teams.get_teams()}
 def verPartidos(request):
-    board = scoreboard.ScoreBoard()
-    result = board.games.get_dict()
-    print (result)
-    games=[]
-    for item in result:
-        games.append(f.format(gameId=item['gameId'], awayTeam=item['awayTeam']['teamName'], score1=item['awayTeam']["score"], score2=item['homeTeam']["score"], homeTeam=item['homeTeam']['teamName']))
+    DatosEnJson = scoreboard.ScoreBoard().games.get_json()
+    data = json.loads(DatosEnJson)
     return render(request, 'index.html', {
-<<<<<<< HEAD
         'games':data})
 
 def verPartidosAyer(request):
@@ -34,16 +18,6 @@ def verPartidosAyer(request):
     ayer = ayer.strftime('%Y-%m-%d')
 
     scoreboard = scoreboardv2.ScoreboardV2(game_date=ayer)
-=======
-        'games':games})
-    
-def verPartidosManana(request):
-    
-    manana = datetime.now() - timedelta(days=1)
-    manana = manana.strftime('%Y-%m-%d')
-
-    scoreboard = scoreboardv2.ScoreboardV2(game_date=manana)
->>>>>>> leandro
 
     partidosDict = scoreboard.get_dict()
 
@@ -55,26 +29,17 @@ def verPartidosManana(request):
         id_partido = partido[2]
         equipo = partido[5]+" "+partido[6]
         puntos = partido[22]
-<<<<<<< HEAD
         logo = f'{equipo.replace(" ", "")}Logo.png'
-=======
-        print(partido)
->>>>>>> leandro
 
         # Si el ID del partido no existe, inicializamos con los datos actuales
         if id_partido not in mapaPartidos:
             mapaPartidos[id_partido] = {
                 "equipo1": equipo,
                 "puntos1": puntos,
-<<<<<<< HEAD
                 "equipo1Logo" : logo,
                 "equipo2": None,
                 "puntos2": None,
                 "equipo2Logo" : None,
-=======
-                "equipo2": None,
-                "puntos2": None,
->>>>>>> leandro
                 "escudoEquipo2" : None,
             }
         else:
@@ -82,14 +47,7 @@ def verPartidosManana(request):
             if mapaPartidos[id_partido]["equipo2"] is None:
                 mapaPartidos[id_partido]["equipo2"] = equipo
                 mapaPartidos[id_partido]["puntos2"] = puntos
-<<<<<<< HEAD
                 mapaPartidos[id_partido]["equipo2Logo"] = logo
     
     return render(request,"ayer.html",{"mapaPartidos": mapaPartidos,"fecha":ayer})
 
-=======
-
-    return render(request,"manana.html",{"mapaPartidos": mapaPartidos,"fecha":manana})
-    
-                    
->>>>>>> leandro
