3b518c16716934f9fb77ed911fc9b461d79e72d1
leandev <romagnolileandro02@gmail.com>
2024-12-29 17:57:03 -0300

    Funcionalidad hoy y mañana

--- mostrarPartidos/urls.py
+++ mostrarPartidos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path ('', views.verPartidos)
+    path ('', views.verPartidos),
+    path ('manana/', views.verPartidosManana)
     
 ]
--- mostrarPartidos/views.py
+++ mostrarPartidos/views.py
@@ -1,9 +1,56 @@
 from django.shortcuts import render
 from nba_api.live.nba.endpoints import scoreboard
-import json
+from nba_api.stats.endpoints import scoreboardv2
+from datetime import datetime, timedelta
+from nba_api.stats.static import teams
+from datetime import date
 # Create your views here.
+f = "{gameId}: {awayTeam} {score1} vs. {score2} {homeTeam}"
+team_dict = {team["id"]: team["full_name"] for team in teams.get_teams()}
 def verPartidos(request):
-    DatosEnJson = scoreboard.ScoreBoard().games.get_json()
-    data = json.loads(DatosEnJson)
+    board = scoreboard.ScoreBoard()
+    result = board.games.get_dict()
+    print (result)
+    games=[]
+    for item in result:
+        games.append(f.format(gameId=item['gameId'], awayTeam=item['awayTeam']['teamName'], score1=item['awayTeam']["score"], score2=item['homeTeam']["score"], homeTeam=item['homeTeam']['teamName']))
     return render(request, 'index.html', {
-        'games':data})
+        'games':games})
+    
+def verPartidosManana(request):
+    
+    manana = datetime.now() - timedelta(days=1)
+    manana = manana.strftime('%Y-%m-%d')
+
+    scoreboard = scoreboardv2.ScoreboardV2(game_date=manana)
+
+    partidosDict = scoreboard.get_dict()
+
+    recorte = partidosDict["resultSets"][1]["rowSet"]
+
+    mapaPartidos = {}
+
+    for partido in recorte:
+        id_partido = partido[2]
+        equipo = partido[5]+" "+partido[6]
+        puntos = partido[22]
+        print(partido)
+
+        # Si el ID del partido no existe, inicializamos con los datos actuales
+        if id_partido not in mapaPartidos:
+            mapaPartidos[id_partido] = {
+                "equipo1": equipo,
+                "puntos1": puntos,
+                "equipo2": None,
+                "puntos2": None,
+                "escudoEquipo2" : None,
+            }
+        else:
+            # Si ya existe, completamos los datos faltantes
+            if mapaPartidos[id_partido]["equipo2"] is None:
+                mapaPartidos[id_partido]["equipo2"] = equipo
+                mapaPartidos[id_partido]["puntos2"] = puntos
+
+    return render(request,"manana.html",{"mapaPartidos": mapaPartidos,"fecha":manana})
+    
+                    
