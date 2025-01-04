7b2f861d12b9b7857fbb4ee198bdf60ddd98df23
leandev <romagnolileandro02@gmail.com>
2025-01-04 10:00:35 -0300

    Correcciones de errores y optimizacion de la implementacion de ver partidor ayer hoy y mañana V2

--- mostrarPartidos/views.py
+++ mostrarPartidos/views.py
@@ -5,34 +5,51 @@ from nba_api.live.nba.endpoints import boxscore
 
 from datetime import datetime, timedelta
 
+import pytz
+from django.utils import timezone
+from datetime import datetime, timedelta
+
 def verPartidos(request):
+    zona_horaria_argentina = pytz.timezone('America/Buenos_Aires')
     board = scoreboard.ScoreBoard()
     result = board.games.get_dict()
-    games=[]
-    for item in result:
-        equipoLocalNombre = item["homeTeam"]["teamCity"]+" "+item["homeTeam"]["teamName"]
-        equipoVisitanteNombre = item["awayTeam"]["teamCity"]+" "+item["awayTeam"]["teamName"]
-        puntosLocal = item["homeTeam"]["score"]
-        puntosVisitante = item["awayTeam"]["score"]
-        logoLocal = f'{equipoLocalNombre.replace(" ", "")}Logo.png'
-        logoVisitante = f'{equipoVisitanteNombre.replace(" ", "")}Logo.png'
-        games.append({
-            "equipoLocalNombre" : equipoLocalNombre,
-            "equipoVisitanteNombre" : equipoVisitanteNombre,
-            "puntosLocal" : puntosLocal,
-            "puntosVisitante" : puntosVisitante,
-            "logoLocal" : logoLocal,
-            "logoVisitante" : logoVisitante,
-            "gameId" : item['gameId']
-        })
+    games={}
+    fecha=datetime.now()
+    condicion= fecha.strftime("%Y-%m-%d")
+    print(fecha.strftime("%H"))
+    print (board.score_board_date)
+    if (board.score_board_date == condicion) | (fecha.strftime("%H") <= "03"):
+        for item in result:
+            equipoLocalNombre = item["homeTeam"]["teamCity"]+" "+item["homeTeam"]["teamName"]
+            equipoVisitanteNombre = item["awayTeam"]["teamCity"]+" "+item["awayTeam"]["teamName"]
+            puntosLocal = item["homeTeam"]["score"]
+            puntosVisitante = item["awayTeam"]["score"]
+            logoLocal = f'{equipoLocalNombre.replace(" ", "")}Logo.png'
+            logoVisitante = f'{equipoVisitanteNombre.replace(" ", "")}Logo.png'
+            id_partido = item['gameId']
+            horario = item['gameTimeUTC']
+            horario = datetime.fromisoformat(horario.replace('Z', '+00:00'))
+            horario = horario.replace(tzinfo=pytz.utc)
+            horario = horario.astimezone(zona_horaria_argentina)
+            fecha_argentina= horario.strftime('%H:%M')
+            games[id_partido]= {
+                "equipo1" : equipoLocalNombre,
+                "equipo2" : equipoVisitanteNombre,
+                "puntos1" : puntosLocal,
+                "puntos2" : puntosVisitante,
+                "equipo1Logo" : logoLocal,
+                "equipo2Logo" : logoVisitante,
+                "fecha_argentina": fecha_argentina,
+            }
+    else:
+        games = obtenerFecha(fecha)
+        
     return render(request, 'index.html', {'games':games})
 
-def verPartidosManana(request):
+def obtenerFecha(fecha:datetime):
+    fecha = fecha.strftime('%Y-%m-%d')
 
-    manana = datetime.now() + timedelta(days=1)
-    manana = manana.strftime('%Y-%m-%d')
-
-    scoreboard = scoreboardv2.ScoreboardV2(game_date=manana)
+    scoreboard = scoreboardv2.ScoreboardV2(game_date=fecha)
 
     partidosDict = scoreboard.get_dict()
 
@@ -43,46 +60,10 @@ def verPartidosManana(request):
     for partido in recorte:
         id_partido = partido[2]
         equipo = partido[5]+" "+partido[6]
-        puntos = partido[22]
-        logo = f'{equipo.replace(" ", "")}Logo.png'
-
-        # Si el ID del partido no existe, inicializamos con los datos actuales
-        if id_partido not in mapaPartidos:
-            mapaPartidos[id_partido] = {
-                "equipo1": equipo,
-                "puntos1": 0,
-                "equipo1Logo" : logo,
-                "equipo2": None,
-                "puntos2": None,
-                "equipo2Logo" : None,
-                "escudoEquipo2" : None,
-            }
+        if partido[22] != None:
+            puntos = partido[22]
         else:
-            # Si ya existe, completamos los datos faltantes
-            if mapaPartidos[id_partido]["equipo2"] is None:
-                mapaPartidos[id_partido]["equipo2"] = equipo
-                mapaPartidos[id_partido]["puntos2"] = 0
-                mapaPartidos[id_partido]["equipo2Logo"] = logo
-
-    return render(request,"manana.html",{"mapaPartidos": mapaPartidos,"fecha":manana})
-
-def verPartidosAyer(request):
-
-    ayer = datetime.now() - timedelta(days=1)
-    ayer = ayer.strftime('%Y-%m-%d')
-
-    scoreboard = scoreboardv2.ScoreboardV2(game_date=ayer)
-
-    partidosDict = scoreboard.get_dict()
-
-    recorte = partidosDict["resultSets"][1]["rowSet"]
-
-    mapaPartidos = {}
-
-    for partido in recorte:
-        id_partido = partido[2]
-        equipo = partido[5]+" "+partido[6]
-        puntos = partido[22]
+            puntos=0
         logo = f'{equipo.replace(" ", "")}Logo.png'
 
         # Si el ID del partido no existe, inicializamos con los datos actuales
@@ -94,8 +75,7 @@ def verPartidosAyer(request):
                 "equipo2": None,
                 "puntos2": None,
                 "equipo2Logo" : None,
-                "escudoEquipo2" : None,
-                "gameId" : id_partido
+                "equipo2Logo" : None,
             }
         else:
             # Si ya existe, completamos los datos faltantes
@@ -103,8 +83,21 @@ def verPartidosAyer(request):
                 mapaPartidos[id_partido]["equipo2"] = equipo
                 mapaPartidos[id_partido]["puntos2"] = puntos
                 mapaPartidos[id_partido]["equipo2Logo"] = logo
-    
-    return render(request,"ayer.html",{"mapaPartidos": mapaPartidos,"fecha":ayer})
+    return mapaPartidos
+
+def verPartidosManana(request):
+
+    manana = datetime.now() + timedelta(days=1)
+    fecha = manana.strftime('%Y-%m-%d')
+    mapaPartidos= obtenerFecha(manana)
+    return render(request,"manana.html",{"mapaPartidos": mapaPartidos,"fecha":fecha})
+
+def verPartidosAyer(request):
+
+    ayer = datetime.now() - timedelta(days=1)
+    fecha = ayer.strftime('%Y-%m-%d')
+    mapaPartidos=obtenerFecha(ayer)
+    return render(request,"ayer.html",{"mapaPartidos": mapaPartidos,"fecha":fecha})
 
 def informacionDetalladaPartido(request,idPartido):
     boxScoreInit = boxscore.BoxScore(idPartido)
